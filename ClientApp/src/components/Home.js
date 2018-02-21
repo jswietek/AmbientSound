7d65b50d8845a53e45facbce92039c43ad8d7347
Jakub Świętek <jakub.swietek@gmail.com>
2018-02-21 11:36:13 +0100

    Fixed formatting for JS files

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import { SoundMasterController } from './sound-master-controller';
 
 export class Home extends Component {
-  displayName = Home.name
+	displayName = Home.name
 
-  render() {
-    return (
-      <div className="container-fluid main">
-        <div className="row" id="sound-master-ctrl-root">
-          <SoundMasterController />
-        </div>
-        <div className="row">
-          <div className="col-xs-12" id="menu-root">
+	render() {
+		return (
+			<div className="container-fluid main">
+				<div className="row" id="sound-master-ctrl-root">
+					<SoundMasterController />
+				</div>
+				<div className="row">
+					<div className="col-xs-12" id="menu-root">
 
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12" id="sound-root">
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12" id="sound-root">
 
-          </div>
-        </div>
-      </div>
-    );
-  }
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
